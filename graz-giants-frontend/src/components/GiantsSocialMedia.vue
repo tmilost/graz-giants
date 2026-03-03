6c3385855f267a1183d2e7c9f2cb6ce6d50cd3c1
Milos Terzic <tmilos94@gmail.com>
2026-03-03 21:57:55 +0100

    feat: refactor social media comp

--- graz-giants-frontend/src/components/GiantsSocialMedia.vue
+++ graz-giants-frontend/src/components/GiantsSocialMedia.vue
@@ -1,5 +1,96 @@
 <template>
-  <div class="giants-social-media" v-html="postContent"></div>
+  <div class="mx-[80px]">
+    <!-- Header row -->
+    <div
+      class="mb-[30px] flex flex-row justify-between max-[430px]:flex-col max-[430px]:items-center"
+    >
+      <p class="text-[50px] font-normal uppercase leading-[39.2px] text-[#003867]">
+        Giants Social Media
+      </p>
+    </div>
+
+    <!-- Social links row -->
+    <div
+      class="flex flex-row items-center justify-between max-[430px]:flex-col max-[430px]:items-center"
+    >
+      <div class="flex flex-row items-center gap-[30px]">
+        <div class="flex items-center justify-center">
+          <img src="@/assets/InstaIconBlue.svg" alt="Icon" />
+
+          <a
+            class="ps-[5px] text-[15px] font-normal leading-[21px] text-[#003867] underline"
+            href="#"
+          >
+            @grazgiants
+          </a>
+        </div>
+        <div class="flex items-center justify-center">
+          <img src="@/assets/InstaIconBlue.svg" alt="Icon" />
+
+          <a
+            class="ps-[5px] text-[15px] font-normal leading-[21px] text-[#003867] underline"
+            href="#"
+          >
+            @grazgiants
+          </a>
+        </div>
+        <div class="flex items-center justify-center">
+          <img src="@/assets/InstaIconBlue.svg" alt="Icon" />
+
+          <a
+            class="ps-[5px] text-[15px] font-normal leading-[21px] text-[#003867] underline"
+            href="#"
+          >
+            @grazgiants
+          </a>
+        </div>
+        <div class="flex items-center justify-center">
+          <img src="@/assets/InstaIconBlue.svg" alt="Icon" />
+
+          <a
+            class="ps-[5px] text-[15px] font-normal leading-[21px] text-[#003867] underline"
+            href="#"
+          >
+            @grazgiants
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <!-- Gallery -->
+    <div class="mt-[50px] flex flex-row flex-wrap justify-center gap-[50px] max-[430px]:gap-[20px]">
+      <img
+        src="@/assets/GameWeek.png"
+        alt="Gallery 1"
+        class="h-[350px] w-[280px] object-cover max-[430px]:h-auto max-[430px]:w-[150px]"
+      />
+      <img
+        src="@/assets/GameWeek.png"
+        alt="Gallery 2"
+        class="h-[350px] w-[280px] object-cover max-[430px]:h-auto max-[430px]:w-[150px]"
+      />
+      <img
+        src="@/assets/GameWeek.png"
+        alt="Gallery 3"
+        class="h-[350px] w-[280px] object-cover max-[430px]:h-auto max-[430px]:w-[150px]"
+      />
+      <img
+        src="@/assets/GameWeek.png"
+        alt="Gallery 4"
+        class="h-[350px] w-[280px] object-cover max-[430px]:h-auto max-[430px]:w-[150px]"
+      />
+      <img
+        src="@/assets/GameWeek.png"
+        alt="Gallery 5"
+        class="h-[350px] w-[280px] object-cover max-[430px]:h-auto max-[430px]:w-[150px]"
+      />
+      <img
+        src="@/assets/GameWeek.png"
+        alt="Gallery 6"
+        class="h-[350px] w-[280px] object-cover max-[430px]:h-auto max-[430px]:w-[150px]"
+      />
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -18,112 +109,3 @@ onMounted(async () => {
   }
 })
 </script>
-
-<style scoped>
-.giants-social-media {
-  margin: 80px;
-}
-
-.giants-social-media:deep(.wp-block-group) {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-}
-
-.giants-social-media:deep(.wp-block-group:first-child > p:nth-child(2)) {
-  display: none;
-}
-
-.giants-social-media:deep(a, a:active) {
-  color: #003867;
-}
-
-.giants-social-media:deep(.wp-block-group > .wp-block-group > .wp-block-image) {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-.giants-social-media:deep(.wp-block-group > .wp-block-group > p) {
-  margin: 0;
-  align-items: center;
-}
-@media screen and (max-width: 430px) {
-  .giants-social-media {
-    margin: 30px;
-  }
-  .giants-social-media:deep(.wp-block-group) {
-    flex-direction: column;
-    align-items: center;
-  }
-
-  .giants-social-media:deep(.wp-block-group > .wp-block-group) {
-    display: flex;
-    flex-direction: row;
-  }
-  .giants-social-media:deep(.wp-block-group p:nth-child(2)) {
-    display: block;
-  }
-}
-
-.giants-social-media:deep(.wp-block-group p:nth-child(1)) {
-  color: #003867;
-  font-size: 28px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 39.2px; /* 140% */
-  text-transform: uppercase;
-}
-
-.giants-social-media:deep(.wp-block-group > .wp-block-group > p) {
-  display: flex;
-  flex-direction: row;
-  width: 120px;
-  padding-left: 5px;
-}
-.giants-social-media:deep(.wp-block-group > .wp-block-group > a),
-.giants-social-media:deep(.wp-block-group > .wp-block-group > a:active) {
-  color: #003867;
-
-  font-size: 15px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 21px; /* 140% */
-  text-decoration-line: underline;
-  text-decoration-style: solid;
-  text-decoration-skip-ink: none;
-  text-decoration-thickness: auto;
-  text-underline-offset: auto;
-  text-underline-position: from-font;
-}
-
-.giants-social-media:deep(.wp-block-gallery) {
-  margin-top: 50px;
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  gap: 50px;
-  justify-content: center;
-}
-
-@media screen and (max-width: 430px) {
-  .giants-social-media:deep(p) {
-    margin-bottom: 30px;
-  }
-
-  .giants-social-media:deep(.wp-block-group:first-child > p:nth-child(2)) {
-    display: block;
-  }
-  .giants-social-media:deep(p),
-  a {
-    text-align: center;
-    justify-content: center;
-  }
-  .giants-social-media:deep(.wp-block-gallery) {
-    gap: 20px;
-  }
-  .giants-social-media:deep(.wp-block-gallery img) {
-    width: 150px;
-    height: auto;
-  }
-}
-</style>
