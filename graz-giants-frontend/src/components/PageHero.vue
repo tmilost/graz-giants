01324273f0696e8e3b4e5def216db07105efd772
Milos Terzic <tmilos94@gmail.com>
2026-03-05 00:25:46 +0100

    feat: fix

---
+++ graz-giants-frontend/src/components/PageHero.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="page-hero">
+    <div class="relative h-[300px] sm:h-[300px] w-full">
+      <img class="block h-full w-full object-cover" :src="imageUrl" alt="Main header image" />
+      <div
+        class="pointer-events-none absolute inset-0 bg-gradient-to-b from-[rgba(0,0,0,0)] from-[-7.32%] to-black to-[102%]"
+      ></div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+})
+</script>
