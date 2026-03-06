5699905e558315128b749247fc53c1e3702b38a6
Milos Terzic <tmilos94@gmail.com>
2026-03-07 00:53:47 +0100

    chore: add reusable button

---
+++ graz-giants-frontend/src/components/ui/BlueButton.vue
@@ -0,0 +1,8 @@
+<template>
+  <button
+    class="inline-flex h-[38px] items-center rounded-[20px] bg-[#003867] px-[20px] text-[15px] font-bold uppercase text-[#FAB900] sm:mt-auto"
+    type="button"
+  >
+    Seasonkarte Bestellen
+  </button>
+</template>
