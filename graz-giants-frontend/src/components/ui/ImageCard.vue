c91a3e85a2f76da27b2bb35d90a2bf9995d47bcc
Milos Terzic <tmilos94@gmail.com>
2026-03-12 03:10:35 +0100

    chore: add Nachwuchs Philosophie

---
+++ graz-giants-frontend/src/components/ui/ImageCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="teams-card h-[270px] w-[270px]">
+    <img class="block h-full w-full object-cover" :src="image" alt="Main header image" />
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  image: {
+    type: String,
+    required: false,
+  },
+})
+</script>
---
+++ graz-giants-frontend/src/views/NachwuchsPhilosophieView.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="nachwuchs-philosophie pb-[50px]">
+    <PageHero :imageUrl="postContent?.image"></PageHero>
+    <div class="px-[80px]">
+      <div class="pt-[30px]">
+        <h1
+          class="text-left text-[50px] font-normal uppercase leading-[50px] text-[#003867] max-[790px]:my-[20px]"
+        >
+          {{ postContent?.tittle }}
+        </h1>
+        <p
+          class="pt-[30px] text-[15px] font-normal leading-[20px] text-[#003867] max-[790px]:my-[20px]"
+        >
+          {{ postContent?.text }}
+        </p>
+        <p
+          class="pt-[30px] text-[28px] font-normal uppercase leading-[28px] text-[#003867] max-[790px]:my-[20px]"
+        >
+          {{ postContent?.card_tittle }}
+        </p>
+      </div>
+      <div class="flex flex-wrap flex-row gap-[80px]">
+        <div
+          class="philosophie-cards pt-[30px]"
+          v-for="(card, index, array) in postContent?.cards"
+          :key="index"
+        >
+          <PhilosophieCard
+            :image="card?.image"
+            :text="card?.text"
+            :class="array % 2 === 1 ? 'bg-[#FAB900] text-[#003867]' : 'text-white'"
+          />
+        </div>
+      </div>
+
+      <p
+        class="pt-[30px] text-[15px] font-normal uppercase leading-[20px] text-[#003867] max-[790px]:my-[20px]"
+      >
+        {{ postContent?.text_bottom }}
+      </p>
+
+      <div class="flex flex-wrap flex-row gap-[30px] mt-[30px]" v-if="postContent?.cards_bottom">
+        <div
+          class="philosophie-cards pt-[30px]"
+          v-for="(card, index) in postContent?.cards_bottom"
+          :key="index"
+        >
+          <ImageCard :image="card?.image" />
+          <p
+            class="pt-[10px] text-[15px] font-normal leading-[20px] text-[#003867] max-[790px]:my-[20px]"
+          >
+            {{ card?.text }}
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+import { useApiCalls } from '../stores/apiCalls.js'
+import PageHero from '@/components/PageHero.vue'
+import BlueButton from '@/components/ui/BlueButton.vue'
+import PhilosophieCard from '@/components/ui/PhilosophieCard.vue'
+import ImageCard from '@/components/ui/ImageCard.vue'
+
+const apiCalls = useApiCalls()
+const postContent = ref({})
+
+onMounted(async () => {
+  postContent.value = await apiCalls.retrievePage('Nachwuchs-Philosophie')
+})
+</script>
