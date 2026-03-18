7f3fcab3c758f007bc98b336864ce8e91675d426
Milos Terzic <tmilos94@gmail.com>
2026-03-18 23:34:46 +0100

    feat: yelow button defoult

---
+++ graz-giants-frontend/src/components/ui/YelowButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <button
+    class="inline-flex h-[38px] items-center rounded-[20px] bg-[#FAB900] px-[20px] text-[15px] font-bold uppercase text-[#003867] sm:mt-auto cursor-pointer"
+    type="button"
+    @click="handleClick"
+  >
+    {{ props.text }}
+  </button>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+const props = defineProps({
+  href: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+})
+const router = useRouter()
+function handleClick() {
+  if (props.href.startsWith('http://') || props.href.startsWith('https://')) {
+    window.open(props.href, '_blank', 'noopener')
+  } else {
+    router.push(props.href)
+  }
+}
+</script>
