b29d0889913aba89a2f08b5619826dbe3180df39
Milos Terzic <tmilos94@gmail.com>
2026-03-20 00:58:43 +0100

    feat: Persist Pinia stores

--- graz-giants-frontend/src/stores/apiCalls.js
+++ graz-giants-frontend/src/stores/apiCalls.js
@@ -3,121 +3,131 @@ import { defineStore } from 'pinia'
 import axios from 'axios'
 import { apiPaths } from '../config/apiPaths'
 
-export const useApiCalls = defineStore('apiCalls', () => {
-  // Simple in-memory cache object
-  const apiCache = {}
+export const useApiCalls = defineStore(
+  'apiCalls',
+  () => {
+    // Simple in-memory cache object
+    const apiCache = ref({})
 
-  onMounted(async () => {})
+    onMounted(async () => {})
 
-  function retrieveHomePageSection(pageSlug) {
-    const cacheKey = `retrieveHomePageSection:${pageSlug}`
-    if (apiCache[cacheKey]) return Promise.resolve(apiCache[cacheKey])
-    return axios
-      .get(`${apiPaths.BASE_API_PATH}/homepage?acf_format=standard&slug=${pageSlug}`)
-      .then((response) => {
-        const data = response.data[0].acf
-        apiCache[cacheKey] = data
-        return data
-      })
-      .catch(() => {
-        return null
-      })
-  }
+    function retrieveHomePageSection(pageSlug) {
+      const cacheKey = `retrieveHomePageSection:${pageSlug}`
+      if (apiCache.value[cacheKey]) return Promise.resolve(apiCache.value[cacheKey])
+      return axios
+        .get(`${apiPaths.BASE_API_PATH}/homepage?acf_format=standard&slug=${pageSlug}`)
+        .then((response) => {
+          const data = response.data[0].acf
+          apiCache.value[cacheKey] = data
+          return data
+        })
+        .catch(() => {
+          return null
+        })
+    }
 
-  function retrieveNews(totalNews) {
-    const cacheKey = `retrieveNews:${totalNews}`
-    if (apiCache[cacheKey]) return Promise.resolve(apiCache[cacheKey])
-    return axios
-      .get(`${apiPaths.BASE_API_PATH}/posts?acf_format=standard&per_page=${totalNews}&order=desc`)
-      .then((response) => {
-        apiCache[cacheKey] = response.data
-        return response.data
-      })
-      .catch(() => {
-        return null
-      })
-  }
+    function retrieveNews(totalNews) {
+      const cacheKey = `retrieveNews:${totalNews}`
+      if (apiCache.value[cacheKey]) return Promise.resolve(apiCache.value[cacheKey])
+      return axios
+        .get(`${apiPaths.BASE_API_PATH}/posts?acf_format=standard&per_page=${totalNews}&order=desc`)
+        .then((response) => {
+          apiCache.value[cacheKey] = response.data
+          return response.data
+        })
+        .catch(() => {
+          return null
+        })
+    }
 
-  function retrieveNewsByTag(tagId) {
-    const cacheKey = `retrieveNewsByTag:${tagId}`
-    if (apiCache[cacheKey]) return Promise.resolve(apiCache[cacheKey])
-    return axios
-      .get(`${apiPaths.BASE_API_PATH}/posts?acf_format=standard&tags=${tagId}&order=desc`)
-      .then((response) => {
-        apiCache[cacheKey] = response.data
-        return response.data
-      })
-      .catch(() => {
-        return null
-      })
-  }
+    function retrieveNewsByTag(tagId) {
+      const cacheKey = `retrieveNewsByTag:${tagId}`
+      if (apiCache.value[cacheKey]) return Promise.resolve(apiCache.value[cacheKey])
+      return axios
+        .get(`${apiPaths.BASE_API_PATH}/posts?acf_format=standard&tags=${tagId}&order=desc`)
+        .then((response) => {
+          apiCache.value[cacheKey] = response.data
+          return response.data
+        })
+        .catch(() => {
+          return null
+        })
+    }
 
-  function retrieveTagsIds(tagSlug) {
-    const slugs = Array.isArray(tagSlug) ? tagSlug.join(',') : tagSlug
-    const cacheKey = `retrieveTagsIds:${slugs}`
-    if (apiCache[cacheKey]) return Promise.resolve(apiCache[cacheKey])
-    return axios
-      .get(`${apiPaths.BASE_API_PATH}/tags?acf_format=standard&slug=${slugs}`)
-      .then((response) => {
-        apiCache[cacheKey] = response.data
-        return response.data
-      })
-      .catch(() => {
-        return null
-      })
-  }
+    function retrieveTagsIds(tagSlug) {
+      const slugs = Array.isArray(tagSlug) ? tagSlug.join(',') : tagSlug
+      const cacheKey = `retrieveTagsIds:${slugs}`
+      if (apiCache.value[cacheKey]) return Promise.resolve(apiCache.value[cacheKey])
+      return axios
+        .get(`${apiPaths.BASE_API_PATH}/tags?acf_format=standard&slug=${slugs}`)
+        .then((response) => {
+          apiCache.value[cacheKey] = response.data
+          return response.data
+        })
+        .catch(() => {
+          return null
+        })
+    }
 
-  function retrieveNewsPost(postSlug) {
-    const cacheKey = `retrieveNewsPost:${postSlug}`
-    if (apiCache[cacheKey]) return Promise.resolve(apiCache[cacheKey])
-    return axios
-      .get(`${apiPaths.BASE_API_PATH}/posts?acf_format=standard&slug=${postSlug}`)
-      .then((response) => {
-        const data = response.data[0]?.acf
-        apiCache[cacheKey] = data
-        return data
-      })
-      .catch(() => {
-        return null
-      })
-  }
+    function retrieveNewsPost(postSlug) {
+      const cacheKey = `retrieveNewsPost:${postSlug}`
+      if (apiCache.value[cacheKey]) return Promise.resolve(apiCache.value[cacheKey])
+      return axios
+        .get(`${apiPaths.BASE_API_PATH}/posts?acf_format=standard&slug=${postSlug}`)
+        .then((response) => {
+          const data = response.data[0]?.acf
+          apiCache.value[cacheKey] = data
+          return data
+        })
+        .catch(() => {
+          return null
+        })
+    }
 
-  function retrievePage(pageSlug) {
-    const cacheKey = `retrievePage:${pageSlug}`
-    if (apiCache[cacheKey]) return Promise.resolve(apiCache[cacheKey])
-    return axios
-      .get(`${apiPaths.BASE_API_PATH}/pages?acf_format=standard&slug=${pageSlug}`)
-      .then((response) => {
-        const data = response.data[0]?.acf
-        apiCache[cacheKey] = data
-        return data
-      })
-      .catch(() => {
-        return null
-      })
-  }
+    function retrievePage(pageSlug) {
+      const cacheKey = `retrievePage:${pageSlug}`
+      if (apiCache.value[cacheKey]) return Promise.resolve(apiCache.value[cacheKey])
+      return axios
+        .get(`${apiPaths.BASE_API_PATH}/pages?acf_format=standard&slug=${pageSlug}`)
+        .then((response) => {
+          const data = response.data[0]?.acf
+          apiCache.value[cacheKey] = data
+          return data
+        })
+        .catch(() => {
+          return null
+        })
+    }
 
-  function retrievePeople(PostType) {
-    const cacheKey = `retrievePeople:${PostType}`
-    if (apiCache[cacheKey]) return Promise.resolve(apiCache[cacheKey])
-    return axios
-      .get(`${apiPaths.BASE_API_PATH}/${PostType}?acf_format=standard&_fields=acf&`)
-      .then((response) => {
-        apiCache[cacheKey] = response.data
-        return response.data
-      })
-      .catch(() => {
-        return null
-      })
-  }
+    function retrievePeople(PostType) {
+      const cacheKey = `retrievePeople:${PostType}`
+      if (apiCache.value[cacheKey]) return Promise.resolve(apiCache.value[cacheKey])
+      return axios
+        .get(`${apiPaths.BASE_API_PATH}/${PostType}?acf_format=standard&_fields=acf&`)
+        .then((response) => {
+          apiCache.value[cacheKey] = response.data
+          return response.data
+        })
+        .catch(() => {
+          return null
+        })
+    }
 
-  return {
-    retrieveHomePageSection,
-    retrieveNews,
-    retrieveNewsPost,
-    retrievePage,
-    retrievePeople,
-    retrieveNewsByTag,
-    retrieveTagsIds,
-  }
-})
+    return {
+      retrieveHomePageSection,
+      retrieveNews,
+      retrieveNewsPost,
+      retrievePage,
+      retrievePeople,
+      retrieveNewsByTag,
+      retrieveTagsIds,
+      apiCache,
+    }
+  },
+  {
+    persist: {
+      storage: sessionStorage,
+      pick: ['apiCache'],
+    },
+  },
+)
