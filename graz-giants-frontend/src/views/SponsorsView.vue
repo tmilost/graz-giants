50cc6a3522fdda29f39ec354d7e0cab81d2bec28
Milos Terzic <tmilos94@gmail.com>
2026-03-19 01:27:45 +0100

    feat: add sponsoring data

--- graz-giants-frontend/src/views/SponsorsView.vue
+++ graz-giants-frontend/src/views/SponsorsView.vue
@@ -1,61 +1,63 @@
 <template>
   <div class="sponsors-page">
-    <PageHero :imageUrl="PageHeroImage" />
+    <PageHero :imageUrl="postContent?.image" />
     <div class="flex flex-col mx-[80px] mt-[30px] mb-[50px]">
       <h2
         class="text-[50px] text-[#003867] font-normal uppercase leading-[50px] text-left mb-[30px]"
       >
-        Sponsoring
+        {{ postContent?.tittle }}
       </h2>
-      <p class="text-[#003867] text-[15px] font-normal leading-[20px]">
-        Die Graz Giants stehen für Leidenschaft, Reichweite und eine starke Community. Als
-        traditionsreicher Verein mit Teams von Nachwuchs bis Kampfmannschaft, Flag Football,
-        Cheerleading und Powerlifting bieten wir Sponsoren eine attraktive Plattform, um ihre Marke
-        emotional, authentisch und zielgruppengenau zu präsentieren. Unsere Heimspiele, Events und
-        digitalen Kanäle erreichen Fans weit über Graz hinaus.
+      <p class="text-[#003867] text-[15px] font-normal leading-[20px] whitespace-pre-wrap">
+        {{ postContent?.text }}
       </p>
-      <p class="text-[#003867] text-[15px] font-normal leading-[20px]">
-        Eine Partnerschaft mit den Graz Giants bedeutet mehr als klassische Werbung: Sie ist ein
-        Bekenntnis zu Teamgeist, Nachwuchsförderung und regionalem Engagement. Gemeinsam entwickeln
-        wir individuelle Sponsoring-Pakete, die Sichtbarkeit schaffen, Werte transportieren und
-        nachhaltige Verbindungen zwischen Unternehmen, Fans und der Giants Family aufbauen.
+      <p class="text-[#003867] text-[28px] font-normal leading-[20px] my-[30px]">
+        {{ postContent?.card_kontaktperson?.tittle }}
       </p>
-      <p class="text-[#003867] text-[28px] font-normal leading-[20px] my-[30px]">Kontaktperson</p>
-      <div class="flex flex-row">
+      <div class="flex flex-col md:flex-row">
         <!-- // to do list -->
         <img
-          src="@/assets/hamburgerIcon.svg"
+          :src="postContent?.card_kontaktperson?.image"
           alt="sponsor image"
           class="w-[220px] h-[261px] object-cover mr-[30px]"
         />
         <div class="flex flex-col">
-          <p class="text-[#003867] text-[15px] font-normal leading-[20px]">Johannes Mörth</p>
           <p class="text-[#003867] text-[15px] font-normal leading-[20px]">
-            Geschäftsführung, Vorstandsmitglied
+            {{ postContent?.card_kontaktperson?.name }}
           </p>
-          <p class="text-[#003867] text-[15px] font-normal leading-[20px]">+43 664 123 45 67</p>
           <p class="text-[#003867] text-[15px] font-normal leading-[20px]">
-            johannes.moerth@grazgiants.at
+            {{ postContent?.card_kontaktperson?.position }}
+          </p>
+          <p class="text-[#003867] text-[15px] font-normal leading-[20px]">
+            {{ postContent?.card_kontaktperson?.phone }}
+          </p>
+          <p class="text-[#003867] text-[15px] font-normal leading-[20px]">
+            {{ postContent?.card_kontaktperson?.email }}
           </p>
         </div>
       </div>
-      <p class="text-[#003867] text-[28px] font-normal leading-[20px] my-[30px]">Kontaktperson</p>
-      <p class="text-[#003867] text-[15px] font-normal leading-[20px]">
-        Die Graz Giants stehen für Leidenschaft, Reichweite und eine starke Community. Als
-        traditionsreicher Verein mit Teams von Nachwuchs bis Kampfmannschaft, Flag Football,
-        Cheerleading und Powerlifting bieten wir Sponsoren eine attraktive Plattform, um ihre Marke
-        emotional, authentisch und zielgruppengenau zu präsentieren. Unsere Heimspiele, Events und
-        digitalen Kanäle erreichen Fans weit über Graz hinaus.
+      <p class="text-[#003867] text-[28px] font-normal leading-[20px] my-[30px]">
+        {{ postContent?.tittle_bottom }}
+      </p>
+      <p class="text-[#003867] text-[15px] font-normal leading-[20px] whitespace-pre-wrap">
+        {{ postContent?.text_bottom }}
       </p>
       <div class="w-fit mt-[30px]">
-        <BlueButton :text="'Seasonkarte Bestellen'" href="#" />
+        <BlueButton :text="postContent?.button?.title" :href="postContent?.button?.url" />
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import PageHeroImage from '@/assets/PageHeroImage.png'
-import PageHero from '../components/PageHero.vue'
+import { onMounted, ref } from 'vue'
+import { useApiCalls } from '../stores/apiCalls.js'
+import PageHero from '@/components/PageHero.vue'
 import BlueButton from '@/components/ui/BlueButton.vue'
+
+const apiCalls = useApiCalls()
+const postContent = ref({})
+
+onMounted(async () => {
+  postContent.value = await apiCalls.retrievePage('sponsoring')
+})
 </script>
