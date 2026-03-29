9c88c6f81aee5fe38108f48f10858be04be25fe8
Milos Terzic <tmilos94@gmail.com>
2026-03-29 23:28:34 +0200

    feat: buttons font

--- graz-giants-frontend/src/components/ui/WhiteButton.vue
+++ graz-giants-frontend/src/components/ui/WhiteButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="font-roboto flex justify-center h-[38px] items-center rounded-[20px] bg-[#FAB900] focus:bg-[#ffffff] px-[20px] text-[15px] font-bold uppercase text-[#003867] hover:text-[#ffffff] focus:text-[#FAB900] sm:mt-auto cursor-pointer"
+    class="font-roboto flex justify-center h-[38px] items-center rounded-[20px] bg-[#FFFFFF] hover:bg-[#FAB900] focus:bg-[#FFFFFF] px-[20px] text-[15px] font-bold uppercase text-[#003867] hover:text-[#ffffff] focus:text-[#FAB900] sm:mt-auto cursor-pointer"
     type="button"
     @click="handleClick"
   >
@@ -12,7 +12,6 @@
 import { useNavigation } from '@/composables/useNavigation'
 
 const { navigate } = useNavigation()
-
 const props = defineProps({
   href: {
     type: String,
@@ -23,6 +22,7 @@ const props = defineProps({
     required: false,
   },
 })
+
 function handleClick() {
   navigate(props.href)
 }
