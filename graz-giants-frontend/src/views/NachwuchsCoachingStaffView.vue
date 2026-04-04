764434ac02517622d1a0376d37c38b202c2c57b7
Milos Terzic <tmilos94@gmail.com>
2026-04-04 23:34:14 +0200

    feat: people by tag

--- graz-giants-frontend/src/views/NachwuchsCoachingStaffView.vue
+++ graz-giants-frontend/src/views/NachwuchsCoachingStaffView.vue
@@ -7,6 +7,19 @@
       >
         {{ heroContent?.tittle }}
       </h1>
+      <!-- Tags -->
+      <div class="font-roboto font-bold flex flex-row items-center gap-[10px] mb-[30px]">
+        <div v-for="(tag, index) in heroContent?.tags" :key="index">
+          <p
+            class="font-roboto text-[15px] font-bold uppercase leading-[15px] leading-[21px] text-[#003867] hover:text-[#FAB900] cursor-pointer"
+            :class="currentTagName === tag ? 'text-[#FAB900] underline' : ''"
+            @click="retrievePeopleByTag(tag)"
+          >
+            {{ tag }}
+          </p>
+        </div>
+      </div>
+
       <!-- Cards -->
       <div class="flex flex-row flex-wrap my-[30px] gap-[30px]">
         <div v-for="(player, index) in postContent" :key="index">
@@ -34,9 +47,33 @@ import RosterCard from '@/components/ui/RosterCard.vue'
 const apiCalls = useApiCalls()
 const postContent = ref({})
 const heroContent = ref({})
+const filteredTagsWithoutSpcesAndDashes = ref([])
+const tagsIds = ref({})
+const currentTagName = ref(null)
 
 onMounted(async () => {
-  postContent.value = await apiCalls.retrievePeople('nwcoachingstaff')
   heroContent.value = await apiCalls.retrievePage('nwcoachingstaff')
+
+  filteredTagsWithoutSpcesAndDashes.value = Object.values(heroContent.value?.tags)?.map((tag) =>
+    tag.replace(/\s+/g, '').replace(/-/g, ''),
+  )
+  tagsIds.value = await apiCalls.retrieveTagsIds(filteredTagsWithoutSpcesAndDashes.value)
+  const firstTagName = Object.values(heroContent.value?.tags)[0]
+  const firstTagObj = tagsIds.value.find((tag) => tag.name === firstTagName)
+  const firstTagId = firstTagObj?.id
+  currentTagName.value = firstTagName
+
+  await apiCalls.retrievePeopleByTag('nwcoachingstaff', firstTagId).then((data) => {
+    postContent.value = data
+  })
 })
+
+function retrievePeopleByTag(tagName) {
+  const tagObj = tagsIds.value.find((tag) => tag.name === tagName)
+  const tagId = tagObj?.id
+  currentTagName.value = tagName
+  apiCalls.retrievePeopleByTag('nwcoachingstaff', tagId).then((data) => {
+    postContent.value = data
+  })
+}
 </script>
